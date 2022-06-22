eb38a72ac0d800ef22b2c7a56a13fb62a5356283
Vinicius Freitas <vinicius_freitas03@hotmail.com>
2022-06-22 13:20:40 -0300

    movie_name ok

--- webscraping/webscrap.py
+++ webscraping/webscrap.py
@@ -5,21 +5,24 @@ import time
 
 browser = webdriver.Chrome("webscraping/chromedriver")
 
-browser.get("https://www.justwatch.com/us/provider/netflix/movies")
+browser.maximize_window()
 
+browser.get('https://www.justwatch.com/us/provider/netflix/movies')
+
+html = browser.page_source 
+
+soup = BeautifulSoup(html, "lxml")
+
+# Scrolling down to the bottom of the page
 browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-
 pre_scroll_height = browser.execute_script('return document.body.scrollHeight;')
-
 run_time, max_run_time = 0, 1
 
 while True:
     iteration_start = time.time()
     # Scroll webpage, the 100 allows for a more 'aggressive' scroll
     browser.execute_script('window.scrollTo(0, 100*document.body.scrollHeight);')
-
     post_scroll_height = browser.execute_script('return document.body.scrollHeight;')
-
     scrolled = post_scroll_height != pre_scroll_height
     timed_out = run_time >= max_run_time
 
@@ -33,24 +36,26 @@ while True:
     elif not scrolled and timed_out:
         break
 
-time.sleep(3)
-
-html = browser.page_source
-
-soup = BeautifulSoup(html, "lxml")
-
-#Creating a list to store the data
-movie_name = []
-
-#Scraping the data
-movie_data = soup.findAll("picture", attrs = {"class":"title-poster__image"})
-
-for div in soup.find_all('picture', 'title-poster__image'):
-    for img in div.find_all('img', alt=True):
-        movie_name.append(img['alt'])
+# Getting the link to obtain the desired data
+links = []
+elems = browser.find_elements_by_xpath("//a[@href]")
+for elem in elems:
+    links.append(elem.get_attribute("href"))
 
 browser.close()
 
+# The first 220 and last 34 links are not useful
+links = links[220:len(links)-34]
+
+# Creating lists to store each and every movie data
+movie_name = []
+
+# Movie names
+for link in links:
+    name = link[35:len(link)]
+    name = name.replace("-", " ").title()
+    movie_name.append(name)
+
 df = pd.DataFrame(movie_name)
 
-df.to_csv('Movie_Names.csv')
+df.to_csv('Movie_Name.csv')
